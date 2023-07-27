25189a41c127495de310de9a6daad770be5a9213
Alexey Kabakov <kabakov_nl@mail.ru>
2023-07-28 01:30:11 +0300

    Выполнено!

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,14 @@ import java.util.List;
 
 @Controller
 public class CarController {
+
+
+    CarServiceImpl carsServiceImpl;
+
+    @Autowired
+    public CarController(CarServiceImpl carsServiceImpl) {
+        this.carsServiceImpl = carsServiceImpl;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
