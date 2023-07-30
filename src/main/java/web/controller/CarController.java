d9c965489fd916745ddd0a8719d2b67711de28e3
Alexey Kabakov <kabakov_nl@mail.ru>
2023-07-30 22:32:01 +0300

    Выполнено!

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
@@ -15,11 +16,11 @@ import java.util.List;
 public class CarController {
 
 
-    CarServiceImpl carsServiceImpl;
+    CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carsServiceImpl) {
-        this.carsServiceImpl = carsServiceImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
